c3f882bcdbdf4aa00fdbb2ccfe4031b36d62f27a
Ken Detweiler <kdetweiler@live.com>
2013-02-10 18:28:43 -0500

    v.04
    char has horizontal and vertical animations. can face directions.

--- island/island/Player.cs
+++ island/island/Player.cs
@@ -14,11 +14,23 @@ namespace island
     class Player : Entity
     {
         AnimationPlayer animationPlayer;
-        Animation walkAnimation;
-        Animation idleAnimation;
+        Animation walkHorizontalRightAnimation;
+        Animation walkHorizontalLeftAnimation;
+        Animation walkVerticalDownAnimation;
+        Animation walkVerticalUpAnimation;
+
+        Animation idleHorizontalRightAnimation;
+        Animation idleHorizontalLeftAnimation;
+        Animation idleVerticalDownAnimation;
+        Animation idleVerticalUpAnimation;
 
         Vector2 position = new Vector2(50, 475);
         Vector2 velocity;
+
+        Boolean lastDirectionUp = false;
+        Boolean lastDirectionDown = false;
+        Boolean lastDirectionHorizontalLeft = false;
+        Boolean lastDirectionHorizontalRight = false;
 
         public Player()
         {
@@ -33,8 +45,15 @@ namespace island
 
         public void Load(ContentManager Content)
         {
-            walkAnimation = new Animation(Content.Load<Texture2D>("walkHorizontal"), 23, 0.1f, true);
-            idleAnimation = new Animation(Content.Load<Texture2D>("idleHorizontal"), 23, 0.3f, true);
+            walkHorizontalRightAnimation = new Animation(Content.Load<Texture2D>("walkHorizontalRight"), 32, 0.1f, true);
+            walkHorizontalLeftAnimation = new Animation(Content.Load<Texture2D>("walkHorizontalLeft"), 32, 0.1f, true);
+            walkVerticalDownAnimation = new Animation(Content.Load<Texture2D>("walkVerticalDown"), 34, 0.1f, true);
+            walkVerticalUpAnimation = new Animation(Content.Load<Texture2D>("walkVerticalUp"), 34, 0.1f, true);
+
+            idleHorizontalRightAnimation = new Animation(Content.Load<Texture2D>("idleHorizontalRight"), 31, 0.3f, true);
+            idleHorizontalLeftAnimation = new Animation(Content.Load<Texture2D>("idleHorizontalLeft"), 31, 0.3f, true);
+            idleVerticalDownAnimation = new Animation(Content.Load<Texture2D>("idleVerticalDown"), 34, .3f, true);
+            idleVerticalUpAnimation = new Animation(Content.Load<Texture2D>("idleVerticalUp"), 34, .3f, true);
         }
 
         public override void Update()
@@ -62,12 +81,56 @@ namespace island
                 velocity.X = 1f;
             else
                 velocity.X = 0f;
+            if (lastDirectionDown == false && lastDirectionUp == false && lastDirectionHorizontalLeft == false && lastDirectionHorizontalRight == false)
+            {
+                animationPlayer.PlayAnimation(idleVerticalDownAnimation);
+            }
 
-            if (velocity.X != 0)
-                animationPlayer.PlayAnimation(walkAnimation);
-            else if (velocity.X == 0)
-                animationPlayer.PlayAnimation(idleAnimation);
+            if (velocity.X < 0 && velocity.Y == 0)
+            {
+                animationPlayer.PlayAnimation(walkHorizontalLeftAnimation);
+                lastDirectionHorizontalLeft = true;
+                lastDirectionHorizontalRight = false;
+                lastDirectionDown = false;
+                lastDirectionUp = false;
+            }
+            else if (velocity.X > 0 && velocity.Y == 0)
+            {
+                animationPlayer.PlayAnimation(walkHorizontalRightAnimation);
+                lastDirectionHorizontalRight = true;
+                lastDirectionHorizontalLeft = false;
+                lastDirectionDown = false;
+                lastDirectionUp = false;
+            }
+            else if (velocity.X == 0 && lastDirectionHorizontalLeft == true)
+                animationPlayer.PlayAnimation(idleHorizontalLeftAnimation);
+            else if (velocity.X == 0 && lastDirectionHorizontalRight == true)
+                animationPlayer.PlayAnimation(idleHorizontalRightAnimation);
 
+            if (velocity.Y > 0 && velocity.X == 0)
+            {
+                animationPlayer.PlayAnimation(walkVerticalDownAnimation);
+                lastDirectionDown = true;
+                lastDirectionHorizontalLeft = false;
+                lastDirectionHorizontalRight = false;
+                lastDirectionUp = false;
+            }
+            else if (velocity.Y < 0 && velocity.X == 0)
+            {
+                animationPlayer.PlayAnimation(walkVerticalUpAnimation);
+                lastDirectionUp = true;
+                lastDirectionHorizontalLeft = false;
+                lastDirectionHorizontalRight = false;
+                lastDirectionDown = false;
+            }
+            else if (velocity.Y == 0 && velocity.X == 0 && lastDirectionDown == true)
+            {
+                animationPlayer.PlayAnimation(idleVerticalDownAnimation);
+            }
+            else if (velocity.Y == 0 && velocity.X == 0 && lastDirectionUp == true)
+            {
+                animationPlayer.PlayAnimation(idleVerticalUpAnimation);
+            }
         }
 
         public void PutInStartPosition()
@@ -80,10 +143,12 @@ namespace island
         {
             SpriteEffects flip = SpriteEffects.None;
 
+            /*
             if (velocity.X >= 0)
                 flip = SpriteEffects.None;
             else if (velocity.X < 0)
                 flip = SpriteEffects.FlipHorizontally;
+            */
 
             animationPlayer.Draw(gameTime, spriteBatch, position, flip);
         }
