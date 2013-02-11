17ed31caaf97fe5c02570937144d53ccb39aed5a
Jason <soulzityr@gmail.com>
2013-02-11 18:03:59 -0500

    .v11b
    Updated Sensors

--- island/island/Sensor.cs
+++ island/island/Sensor.cs
@@ -18,10 +18,10 @@ namespace island
         public static int length;
         public float distance;
 
-        //Proximity Sensors
+        //Collision Sensors
         const int proximityMargin = 5;
 
-        //Adjacency Sensors
+        //Adjacency(Proximity) Sensors
 
         //The owner of this WallSensor
         public static int sightRange; //how far this particular owner can see
@@ -32,6 +32,7 @@ namespace island
             length = pi_division;
         }
 
+        //Proximity Sensor
         public void Proximity(Player owner, int radius, List<NPC> npcs)
         {
             List<NPC> prox = new List<NPC>();
@@ -49,51 +50,6 @@ namespace island
             owner.proxList = prox;
         }
 
-        /*
-        public void proximitySensor(Rectangle rectangle, int radius, List<Npc> npcs)
-        {
-            for(int k=0;k<=360;k+=45) {
-                for each Npc in Npc
-            }
-            
-        }*/
-
-        public void WallsGetAll(Rectangle owner, Vector2 location, Vector2 direction, List<NPC> charlist, List<GameObject> objectList)
-        {
-            for (int k = 0; k < length; k++) { 
-                WallScanner[k]=WallsDetect(owner, location, direction, charlist, objectList);
-            }
-        }
-
-        public static float? WallsDetect(Rectangle owner, Vector2 location, Vector2 direction, List<NPC> charlist, List<GameObject> objectList)
-        {
-            Vector3 newLocation = new Vector3(location.X, location.Y, 0);
-            Vector3 newDirection = new Vector3(direction.X, direction.Y, 0);
-            float? ans = -1;
-            Ray ray = new Ray(newLocation, newDirection);
-            //look in direction for sightRange, if a wall detected, return how far away it is
-            //else return -1, indicating that no wall is in that direction
-
-            for (int k = 0; k < charlist.Count; k++) { 
-                NPC temp=charlist[k];
-                Vector3 tempMin = new Vector3(temp.position.X,temp.position.Y,0);
-                BoundingBox tempBox = new BoundingBox(tempMin, tempMin);
-                if(ray.Intersects(tempBox)<=sightRange && ray.Intersects(tempBox)<ans) {
-                    ans= ray.Intersects(tempBox);
-                }
-            }
-            for (int k = 0; k < objectList.Count; k++)
-            {
-                GameObject temp = objectList[k];
-                Vector3 tempMin = new Vector3(temp.position.X, temp.position.Y, 0);
-                BoundingBox tempBox = new BoundingBox(tempMin, tempMin);
-                if (ray.Intersects(tempBox) <= sightRange && ray.Intersects(tempBox)<ans)
-                {
-                    ans= ray.Intersects(tempBox);
-                }
-            }
-            //if (ray.Intersects()) { }
-            return ans;
-        }
+        
     }
 }
