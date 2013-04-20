3df3f18f9e4a85e13c819220307f21c587ecb3d6
Jason <soulzityr@gmail.com>
2013-04-20 16:56:48 -0400

    4.7
    made Weapon

--- island/island/Weapon.cs
+++ island/island/Weapon.cs
@@ -2,6 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 
 namespace island
 {
@@ -22,7 +29,7 @@ namespace island
 
         public Boolean inSweetSpot(NPC target) 
         {
-
+            Vector2 spot=target.position;
             for (int k = 0; k < sweetSpots.Length; k++) 
             { 
                 
